adf40d178950bdebb59772fe0439fc80f9f26a08
krupesh220622 <krupeshpansara@gmail.com>
2022-12-10 12:55:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import MediaMessages from "./Component/MediaMessages";
+import axios from "axios";
+import CustomMessage from "./Component/CustomMessage";
+// import LocationMessages from "./Component/LocationMessages";
 
 function App() {
+
+  const SendData = (ConfigData)=>{
+    var data = JSON.stringify(ConfigData);
+
+    var config = {
+      method: "post",
+      url: "https://graph.facebook.com/v15.0/111412271813097/messages",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer EAAGp6OmGBZCUBAO7PH0phmdOcLK1mukrmfk3znJ5H4vNq8m08ZAirJD5enT1Mumyf5xCeM6l790yzDmmcFrrYbKf6OwfN5Nx8ZCDZAAV56rGEVnQ6hdOwxdSYlwvS7rd6hIp7cL329fpseYCTEgtLnmrJAGjob0hyYRpzfRF2eZCGpmvKECWrFAlB1ut8VpV5bJXGL6uwMAZDZD",
+      },
+      data: data,
+    };
+
+     axios(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CustomMessage SendFunction = {SendData}/>
+      {/* <MediaMessages SendFunction = {SendData}/> */}
+      {/* <LocationMessages/> */}
+    </>
   );
 }
 
